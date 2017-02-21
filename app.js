28809668f8fd55b640d5e295caccd89384a57873
ALOULOU <mhiria@yahoo.fr>
2017-02-21 23:43:05 +0100

    TP3 ALI version finale

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.set( 'view engine', 'ejs' );
 //setting the 'assets' directory as our static assets dir (css, js, img, etc...)
 app.use( '/assets', express.static( 'assets' ) );
 
+/*
 var data = {
     prix: 0,
     type: "",
@@ -26,7 +27,7 @@ var data = {
     adresse: "",
     prixSurfaceHabitable: 0,
 }
-
+*/
 
 //makes the server respond to the '/' route and serving the 'home.ejs' template in the 'views' directory
 //var url = "https://www.leboncoin.fr/ventes_immobilieres/1074660822.htm?ca=12_s"
@@ -39,63 +40,118 @@ app.get( '/', function ( req, res ) {
             if ( !error && response.statusCode == 200 ) {
                 var $ = cheerio.load( body );
                 {
-                    //le trim enlève tous les espaces au début et à la fin
-
+                    /*le trim enlève tous les espaces au début et à la fin*/
                     var prixx = $( 'h2.item_price.clearfix span.value ' ).text().trim().split( ' ' );
-                    data.prix = prixx[0] + prixx[1];
 
-                    var typee = $( $( 'h2.clearfix span.value' ).get( 3 ) ).text()
-                    data.type = typee
+                    /*parseInt pour price et surface pour faire le rapport des deux
+                    et obtenir le prixAuMetreCarre sinon price et surface sont considérés
+                    comme deux string */
+                    var price = parseInt( prixx[0] + prixx[1] );
 
-                    var piecess = $( $( 'h2.clearfix span.value' ).get( 4 ) ).text()
-                    data.pieces = piecess
+                    /*Dans les annonces, les frais d'agences peuvent être inclus ou non.
+                    Ce if/else me permet de distinguer ce cas là.*/
+                    if ( $( $( 'h2.clearfix span.value' ).get( 2 ) ).text() == 'Oui' ) {
+                        var type = $( $( 'h2.clearfix span.value' ).get( 3 ) ).text()
+                        var pieces = $( $( 'h2.clearfix span.value' ).get( 4 ) ).text()
+                        var surface = parseInt( $( $( 'h2.clearfix span.value' ).get( 5 ) ).text() )
+                        var classeEnergie = $( $( 'h2.clearfix span.value' ).get( 8 ) ).text()
+                    }
+                    else {
+                        var type = $( $( 'h2.clearfix span.value' ).get( 2 ) ).text()
+                        var pieces = $( $( 'h2.clearfix span.value' ).get( 3 ) ).text()
+                        var surface = parseInt( $( $( 'h2.clearfix span.value' ).get( 4 ) ).text() )
+                        var classeEnergie = $( $( 'h2.clearfix span.value' ).get( 6 ) ).text()
+                    }
 
-                    var surfacee = $( $( 'h2.clearfix span.value' ).get( 5 ) ).text()
-                    data.surface = surfacee
+                    /*Je choisi ici d'afficher l'adresse complètement sans distinguer ville et code postal
+                    car parfois les villes peuvent être composées, d'où ce choix*/
+                    var adresse = $( $( 'h2.clearfix span.value' ).get( 1 ) ).text()
+                    var ville = adresse.split( ' ' )[0];
+                    var codePostal = adresse.split( ' ' )[1];
 
-                    //var adresse = $( $( 'h2.clearfix span.value' ).get( 3 ) ).text()
-                    data.adresse = $( $( 'h2.clearfix span.value' ).get( 1 ) ).text()
+                    /*Prix au m² = Ratio du price sur la surface prélablement convertis en int*/
+                    var prixAuMetreCarre = price / surface;
 
-
-                    var ville = data.adresse.split( ' ' )[0];
-                    var codePostal = data.adresse.split( ' ' )[1];
-
-                    //data.cp = $( $( 'h2.clearfix span.value' ).get( 0 ) ).text()
-
-                    data.prixAuMetreCarre = parseInt( data.prix ) / parseInt( data.surface )
                 }
-                res.render( 'home', {
-                    prix: data.prix,
-                    type: data.type,
-                    pieces: data.pieces,
-                    surface: data.surface,
-                    adresse: data.adresse,
-                    prixAuMetreCarre: data.prixAuMetreCarre,
-                });
+
+
+                request( 'http://www.meilleursagents.com/prix-immobilier/' + ville.toLowerCase() + '-' + codePostal, function ( error, response, body ) {
+                    if ( !error && response.statusCode == 200 ) {
+
+                        var dollar = cheerio.load( body );
+
+                        /*On charge la colonne qui contient les prix moyen du m² */
+                        var prixco = dollar( 'div.small-4.medium-2.columns.prices-summary__cell--median' );
+
+                        /*Distinction de cas : si c'est un Appartement on prend la première valeur de la colonne,
+                        et si c'est une Maison on prend la deuxième*/
+                        if ( type == "Appartement" ) {
+
+                            var prixComparateur = prixco.eq( 0 ).html().split( '&#xA0;' );
+
+                            /*parseInt pour pouvoir ensuite comparer les 2 variables prixComparateur
+                            et prixAuMetreCarre*/
+                            var priceComparateur = parseInt( prixComparateur[0] + prixComparateur[1] );
+
+                            if ( prixAuMetreCarre <= priceComparateur ) {
+                                var conclusion = "bonne";
+                            }
+                            else {
+                                var conclusion = "mauvaise";
+                            }
+
+                        }
+
+                        if ( type == "Maison" ) {
+
+                            var prixComparateur = prixco.eq( 1 ).html().split( '&#xA0;' );
+                            var priceComparateur = parseInt( prixComparateur[0] + prixComparateur[1] );
+
+                            if ( prixAuMetreCarre <= priceComparateur ) {
+                                var conclusion = "bonne";
+                            }
+                            else {
+                                var conclusion = "mauvaise";
+                            }
+
+                        }
+
+                        res.render( 'home', {
+                            prix: price,
+                            type: type,
+                            pieces: pieces,
+                            surface: surface,
+                            adresse: adresse,
+                            prixAuMetreCarre: prixAuMetreCarre,
+                            classeEnergie: classeEnergie,
+                            prixComparateur: priceComparateur,
+                            conclusion: conclusion,
+                        });
+
+
+                    }
+
+                })
             }
-
-
-            request( 'http://www.meilleursagents.com/prix-immobilier/' + ville.toLowerCase() + '-' + codePostal, function ( error, response, body ) {
-                if ( !error && response.statusCode == 200 ) {
-                    var $ = cheerio.load( body );
-
-                }
-
-            })
 
 
         })
     }
+
     else {
         res.render( 'home', {
-            prix: data.prix,
-            type: data.type,
-            pieces: data.pieces,
-            surface: data.surface,
-            adresse: data.adresse,
-            prixAuMetreCarre: data.prixAuMetreCarre,
+            prix: 0,
+            type: "",
+            pieces: 0,
+            surface: 0,
+            adresse: "",
+            prixAuMetreCarre: 0,
+            classeEnergie: "",
+            prixComparateur: 0,
+            conclusion: "",
         });
     }
+
 });
 
 
